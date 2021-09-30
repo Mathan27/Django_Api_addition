2ee660a176641933b88c647ef6097bef73672816
Mathankumar <mathanrg27@gmail.com>
2021-09-30 21:57:49 +0530

    second commit

--- myapp/views.py
+++ myapp/views.py
@@ -3,25 +3,19 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
 from myapp.serializers import add
-
 from django.views.decorators.csrf import csrf_exempt
-
 from rest_framework.parsers import JSONParser
-
 
 # Create your views here.
 @csrf_exempt
 def addition(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        a=data["a"]
-        b=data["b"]
+        d= dict(data)
         res = {"data":0}
-        serializer = add(data=data) 
-        if serializer.is_valid():
-            c=a+b
-            res["data"]=c
-            #json_obj= json.loads(str(res))
-            return JsonResponse(res)
-            #return HttpResponse(c)
-        return JsonResponse(serializer.errors,status=400)
+        s=0
+        for key,value in d.items():
+            if(int == type(value)):
+                s=s+value
+        res["data"]=s
+        return JsonResponse(res)
